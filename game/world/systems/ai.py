3c1d8ad7e84de6ebe0adae45c6e6d1dd690c1bf1
Kleob Loflin <kll0032@uah.edu>
2025-09-25 12:10:11 -0500

    ai.py cleaned up

--- game/world/systems/ai.py
+++ game/world/systems/ai.py
@@ -32,89 +32,131 @@ class EnemyAISystem:#System):
         # chase entities
         for entity_id, comps in world.query(Transform, Intent, AI): #possible later addition WonderAI
             ai: AI = comps[AI]
+            pos: Transform = comps[Transform]
+            intent: Intent = comps[Intent]
 
+            
+            target_pos: Transform = world.get(ai.target_id, Transform)
+            if pos is None or target_pos is None:
+                continue
+
+               
+            dx = target_pos.x - pos.x
+            dy = target_pos.y - pos.y
+            dist = (dx * dx + dy * dy) ** 0.5
             # only handle chase entities
             if ai.kind != "chase":
-                continue
+                if dist > 10 and dist < ai.agro_range:  
+                    intent.move_x = dx / dist
+                    intent.move_y = dy / dist
+                else:
+                    intent.move_x = 0.0
+                    intent.move_y = 0.0
 
-            pos: Transform = comps[Transform]
-            intent: Intent = comps[Intent]
+            elif ai.kind != "flee":
             
-            target_pos: Transform = world.get(ai.target_id, Transform)
-            if pos is None or target_pos is None:
-                continue
+                if dist < ai.agro_range and dist > 10:
+                    intent.move_x = -dx / dist
+                    intent.move_y = -dy / dist
+                else:
+                    intent.move_x = 0.0
+                    intent.move_y = 0.0
 
-            dx = target_pos.x - pos.x
-            dy = target_pos.y - pos.y
-            dist = (dx * dx + dy * dy) ** 0.5
-
-            #THIS IS AGRO RANGE WE NEED TO CHANGE IT SO THAT IT WONT FOLLOW ABOVE RANGE
-            if dist > 10 and dist < ai.agro_range:  
-                intent.move_x = dx / dist
-                intent.move_y = dy / dist
-            else:
-                intent.move_x = 0.0
-                intent.move_y = 0.0
-
+            elif ai.kind != "wander":
+                dx = random.randint(-10, 10)
+                dy = random.randint(-10, 10)
+            
+                if dist < 200 and dist > 0:
+                    intent.move_x = -dx / dist
+                    intent.move_y = -dy / dist
+                else:
+                    intent.move_x = 0.0
+                    intent.move_y = 0.0
+            
             # facing
             if intent.move_x < -0.01:
-                intent.facing = "left"
+                    intent.facing = "left"
             elif intent.move_x > 0.01:
-                intent.facing = "right"
+                    intent.facing = "right"
 
+
+
+           #ADD A PROJECTILE
+           # CODE TO FIND DISTANCE AND GO IN STRAIGHT ORDER.
+           # 
+           # ADD A TRAP 
+            
+
+            #dx = target_pos.x - pos.x
+            #dy = target_pos.y - pos.y
+            #dist = (dx * dx + dy * dy) ** 0.5
+
+            #THIS IS AGRO RANGE WE NEED TO CHANGE IT SO THAT IT WONT FOLLOW ABOVE RANGE
+            #if dist > 10 and dist < ai.agro_range:  
+            #    intent.move_x = dx / dist
+            #    intent.move_y = dy / dist
+            #else:
+            #    intent.move_x = 0.0
+            #    intent.move_y = 0.0
+
+            # facing
+            #if intent.move_x < -0.01:
+            #    intent.facing = "left"
+            #elif intent.move_x > 0.01:
+            #    intent.facing = "right"
 
         # FLEE entities
-        for entity_id, comps in world.query(Transform, Intent, AI):
-            ai: AI = comps[AI]
+#         for entity_id, comps in world.query(Transform, Intent, AI):
+#             ai: AI = comps[AI]
             
-            # only handle flee entities
-            if ai.kind != "flee":
-                continue
+#             # only handle flee entities
+#             if ai.kind != "flee":
+#                 continue
 
-            pos: Transform = comps[Transform]
-            intent: Intent = comps[Intent]
+#             #pos: Transform = comps[Transform]
+#             #intent: Intent = comps[Intent]
             
             
-            target_pos: Transform = world.get(ai.target_id, Transform)
-            if pos is None or target_pos is None:
-                continue
-
-            dx = target_pos.x - pos.x
-            dy = target_pos.y - pos.y
-            dist = (dx * dx + dy * dy) ** 0.5
-
-
-            #THIS IS AGRO RANGE agro_range
-            if dist < ai.agro_range and dist > 10:
-                intent.move_x = -dx / dist
-                intent.move_y = -dy / dist
-            else:
-                intent.move_x = 0.0
-                intent.move_y = 0.0
+#             #target_pos: Transform = world.get(ai.target_id, Transform)
+#             #if pos is None or target_pos is None:
+#             #    continue
+# #
+#             dx = target_pos.x - pos.x
+#             dy = target_pos.y - pos.y
+#             dist = (dx * dx + dy * dy) ** 0.5
 
 
+#             #THIS IS AGRO RANGE agro_range
+#             if dist < ai.agro_range and dist > 10:
+#                 intent.move_x = -dx / dist
+#                 intent.move_y = -dy / dist
+#             else:
+#                 intent.move_x = 0.0
+#                 intent.move_y = 0.0
 
-        # WONDER entities this crashes everything
-        for entity_id, comps in world.query(Transform, Intent, AI):
-            ai: AI = comps[AI]
 
-            # only handle wonder entities
-            if ai.kind != "wander":
-                continue
+
+#         # WONDER entities this crashes everything
+#         for entity_id, comps in world.query(Transform, Intent, AI):
+#             ai: AI = comps[AI]
+
+#             # only handle wonder entities
+#             if ai.kind != "wander":
+#                 continue
             
-            pos: Transform = comps[Transform]
-            intent: Intent = comps[Intent]
+#             #pos: Transform = comps[Transform]
+#             #intent: Intent = comps[Intent]
 
-            #time.sleep(4 )  crashes the shit out of everything  
-            dx = random.randint(-10, 10)
-            dy = random.randint(-10, 10)            
+#             #time.sleep(4 )  crashes the shit out of everything  
+#             dx = random.randint(-10, 10)
+#             dy = random.randint(-10, 10)            
             
-            dist = (dx * dx + dy * dy) ** 0.5
+#             dist = (dx * dx + dy * dy) ** 0.5
 
-            #ONCE AGAIN AGRO RANGE
-            if dist < 200 and dist > 0:
-                intent.move_x = -dx / dist
-                intent.move_y = -dy / dist
-            else:
-                intent.move_x = 0.0
-                intent.move_y = 0.0
+#             #ONCE AGAIN AGRO RANGE
+#             if dist < 200 and dist > 0:
+#                 intent.move_x = -dx / dist
+#                 intent.move_y = -dy / dist
+#             else:
+#                 intent.move_x = 0.0
+#                 intent.move_y = 0.0
