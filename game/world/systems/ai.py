89366f724d183dd3135469bfe8d2469b9e57977b
Kleob Loflin <kll0032@uah.edu>
2025-10-05 13:48:54 -0500

    project.json creation and wrking on

--- game/world/systems/ai.py
+++ game/world/systems/ai.py
@@ -72,7 +72,25 @@ class EnemyAISystem:#System):
                 else:
                     intent.move_x = 0.0
                     intent.move_y = 0.0
-            
+            #elif ai.kind == "straight_line":
+            #    intent.move_x = dx / dist
+            #    intent.move_y = dy / dist
+
+                #BELOW THIS needs to be worked on
+            #elif ai.kind == "ranged" and target_pos:
+            #    ai.max_cooldown -= dt
+            #    if 10 < dist < ai.agro_range and ai.max_cooldown <= 0:
+            #        ai.max_cooldown = ai.max_cooldown  # reset attack cooldown
+
+        # spawn projectile
+            #proj_id = world.new_entity()
+            #bp = enemy_bp("projectile.arrow")  # blueprint for arrow
+            #ctx = {"pos": (pos.x, pos.y), "owner": entity_id, "target_id": ai.target_id}
+            #apply_blueprint(world, proj_id, bp, ctx)
+
+        # optional: face target
+            #intent.facing = "left" if dx < 0 else "right
+
             # facing
             if intent.move_x < -0.01:
                     intent.facing = "left"
@@ -83,80 +101,9 @@ class EnemyAISystem:#System):
 
            #ADD A PROJECTILE
            # CODE TO FIND DISTANCE AND GO IN STRAIGHT ORDER.
-           # 
+           # if range
+           #delete after timer
+
+
            # ADD A TRAP 
             
-
-            #dx = target_pos.x - pos.x
-            #dy = target_pos.y - pos.y
-            #dist = (dx * dx + dy * dy) ** 0.5
-
-            #THIS IS AGRO RANGE WE NEED TO CHANGE IT SO THAT IT WONT FOLLOW ABOVE RANGE
-            #if dist > 10 and dist < ai.agro_range:  
-            #    intent.move_x = dx / dist
-            #    intent.move_y = dy / dist
-            #else:
-            #    intent.move_x = 0.0
-            #    intent.move_y = 0.0
-
-            # facing
-            #if intent.move_x < -0.01:
-            #    intent.facing = "left"
-            #elif intent.move_x > 0.01:
-            #    intent.facing = "right"
-
-        # FLEE entities
-#         for entity_id, comps in world.query(Transform, Intent, AI):
-#             ai: AI = comps[AI]
-            
-#             # only handle flee entities
-#             if ai.kind != "flee":
-#                 continue
-
-#             #pos: Transform = comps[Transform]
-#             #intent: Intent = comps[Intent]
-            
-            
-#             #target_pos: Transform = world.get(ai.target_id, Transform)
-#             #if pos is None or target_pos is None:
-#             #    continue
-# #
-#             dx = target_pos.x - pos.x
-#             dy = target_pos.y - pos.y
-#             dist = (dx * dx + dy * dy) ** 0.5
-
-
-#             #THIS IS AGRO RANGE agro_range
-#             if dist < ai.agro_range and dist > 10:
-#                 intent.move_x = -dx / dist
-#                 intent.move_y = -dy / dist
-#             else:
-#                 intent.move_x = 0.0
-#                 intent.move_y = 0.0
-
-
-
-#         # WONDER entities this crashes everything
-#         for entity_id, comps in world.query(Transform, Intent, AI):
-#             ai: AI = comps[AI]
-
-#             # only handle wonder entities
-#             if ai.kind != "wander":
-#                 continue
-            
-#             #pos: Transform = comps[Transform]
-#             #intent: Intent = comps[Intent]
-
-#             #time.sleep(4 )  crashes the shit out of everything  
-#             dx = random.randint(-10, 10)
-#             dy = random.randint(-10, 10)            
-            
-#             dist = (dx * dx + dy * dy) ** 0.5
-
-#             #ONCE AGAIN AGRO RANGE
-#             if dist < 200 and dist > 0:
-#                 intent.move_x = -dx / dist
-#                 intent.move_y = -dy / dist
-#             else:
-#                 intent.move_x = 0.0
-#                 intent.move_y = 0.0
