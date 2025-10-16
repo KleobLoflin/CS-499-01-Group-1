e6c015fbcbf7fe1c959c0efaeba12c52437489e1
ScottWPetty <scottwilsonpetty@gmail.com>
2025-10-16 00:04:16 -0500

    added blueprint and atlas information for blue/green/red/yellow/purple knight

--- game/scenes/dungeon.py
+++ game/scenes/dungeon.py
@@ -48,7 +48,7 @@ class DungeonScene(Scene):
         # Spawn player once here (or set SpawnPolicy.spawn_player=True to use blueprint)
         self.player_id = create_hero(
             self.world,
-            archetype="knight",
+            archetype="knight_yellow",
             owner_client_id=None,
             pos=(Config.WINDOW_W/2 - 16, Config.WINDOW_H/2 - 16)
         )
